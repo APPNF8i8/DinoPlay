5381581666dfcdc5f55f089f271474da21e5978c
Flavio <50881724+APPNF8i8@users.noreply.github.com>
2021-12-06 15:04:44 -0300

    Update dino try 1.py

--- dino try 1.py
+++ dino try 1.py
@@ -3,32 +3,29 @@ from PIL import ImageGrab, ImageDraw
 from datetime import datetime
 
 if __name__ == "__main__":
-  
-     # 1st split your monitor with 2 screens, 1 with python code and other with Dino game
-     # Afte press play in the code, select dino screen to activate it
-    print("Click on screen window to start")   
-    pyautogui.write('  ', interval=1) 
-    
-    scr_position = [860, 280]
+
+    # 1st split your monitor with 2 screens, 1 with python code and other with Dino game
+    # After press play in the code, select dino screen to activate it
+    print("Click on screen window to start")
+
+    scr_position = [845, 280]
     area = (scr_position[0], scr_position[1], scr_position[0] + 300, scr_position[1] + 170)
 
     while True:
         dino_capture = ImageGrab.grab()
         cropped_img = dino_capture.crop(area)
         cropped_img.convert('1')
-        crop_cactus = cropped_img.crop((143,80,190,110))
-        ptero = cropped_img.getpixel((193, 50))
+        crop_cactus = cropped_img.crop((158, 80, 215, 110))
+        crop_passed = cropped_img.crop((5, 80, 18, 110))
+
         colors_cactus = crop_cactus.getcolors()
+        colors_passed = crop_passed.getcolors()
 
-        print(" - ptero - ", ptero, ' - rect cactus - ',colors_cactus)
+        print(datetime.now().strftime("%S-%f"), "-  passed - ",colors_passed[0][0], ' - rect cactus - ', colors_cactus)
 
-        if colors_cactus[0][0] <1350:
+        if colors_cactus[0][0] < 1700:
             pyautogui.keyDown('space')
-            print("--------- CACTUS ---------")
-
-        draw = ImageDraw.Draw(cropped_img)
-        draw.rectangle((143,80,190,110), fill=None, outline='green', width=1)
-
-        date_string = datetime  .now().strftime("%H-%M-%S-%f")
-        date_string = 'shots/' +date_string + ".jpg"
-        cropped_img.save(date_string, "JPEG")
+            print("------- CACTUS -------")
+        if colors_passed[0][0] < 380:
+            pyautogui.keyDown('down')
+            pyautogui.keyUp('down')
